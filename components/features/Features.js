6a264e19780dbc744b5ae471912f96399f1ed309
yardiewizardie <yardiewizardie@gmail.com>
2019-09-24 18:21:03 -0400

    added some SVGs

--- components/features/Features.js
+++ components/features/Features.js
@@ -1,48 +1,42 @@
 import React from 'react';
 import './features.scss';
 
+import dice from '../../static/icons/dice.svg';
+import bard from '../../static/icons/bardicon.svg';
+import cleric from '../../static/icons/clericicon.svg';
+import druid from '../../static/icons/druidicon.svg';
+import fighter from '../../static/icons/fightericon.svg';
+import wizard from '../../static/icons/wizardicon.svg';
 
 export default () => (
     <section className="features">
         <div className="feature">
-            <svg className="feature__icon">
-                <use />
-            </svg>
+            <img src={dice} className="feature__icon" />
             <h4 className="feature__title">World's Famous dice</h4>
             <p className="feature__text">Join the handcrafted </p>
         </div>
         <div className="feature"> <div className="feature">
-            <svg className="feature__icon">
-                <use />
-            </svg>
+           <img src={bard} className="feature__icon" />
             <h4 className="feature__title">Homebrew Made</h4>
             <p className="feature__text">Made with passion and alot of stuff that I have placeholder </p>
         </div></div>
         <div className="feature"> <div className="feature">
-            <svg className="feature__icon">
-                <use />
-            </svg>
+           <img src={cleric} className="feature__icon" />
             <h4 className="feature__title">Tailorcrafted Bag</h4>
             <p className="feature__text">All set of dices come with a special tailorcrafted bag, made by the great dookie</p>
         </div></div>
         <div className="feature"> <div className="feature">
-            <svg className="feature__icon">
-                <use />
-            </svg>
+           <img src={druid} className="feature__icon" />
             <h4 className="feature__title">Colorful & Resillences</h4>
             <p className="feature__text">You can pick from quite variations of sets, make your choice!</p>
         </div></div>
         <div className="feature"> <div className="feature">
-            <svg className="feature__icon">
-                <use />
-            </svg>
+           <img src={wizard} className="feature__icon" />
             <h4 className="feature__title">Plastic Made</h4>
             <p className="feature__text">Our dices are made with the best quality of POXICA you can find</p>
         </div></div>
         <div className="feature"> <div className="feature">
-            <svg className="feature__icon">
-                <use />
-            </svg>
+           <img src={fighter} className="feature__icon" />
             <h4 className="feature__title">Lawful Good Dwarf</h4>
             <p className="feature__text">Mama Berissa is a lawful good dwarf priest who has helped alot of people</p>
         </div></div>
--- next.config.js
+++ next.config.js
@@ -2,16 +2,20 @@ const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
 const withFonts = require('next-fonts')
 const withImages = require('next-images')
+const withReactSvg = require('next-react-svg')
+const path = require('path')
 
 module.exports = withImages(
     withFonts(
         withCSS(
-            withSass({
-                enableSvg: true,
-                webpack(config, options) {
-                    return config
-                },
-            })
+            withSass(
+                withReactSvg({
+                    include: path.resolve(__dirname, 'src/static/icons'),
+                    webpack(config, options) {
+                      return config
+                    }
+                  })
+            )
         )
     )
 )
