0a4593aff9daef2f086b62c16b2bd5db2bcd0284
yardiewizardie <yardiewizardie@gmail.com>
2019-09-25 01:31:18 -0400

    added dots and text bubble

--- components/features/Features.js
+++ components/features/Features.js
@@ -13,32 +13,53 @@ export default () => (
         <div className="feature">
             <img src={dice} className="feature__icon" />
             <h4 className="feature__title">World's Famous dice</h4>
-            <p className="feature__text">Join the handcrafted </p>
+            <div className="talk-bubble tri-right round left-in">
+                <div className="talktext">
+                    <p className="feature__text">Join the handcrafted </p>
+                </div>
+            </div>
         </div>
         <div className="feature"> <div className="feature">
            <img src={bard} className="feature__icon" />
             <h4 className="feature__title">Homebrew Made</h4>
+            <div className="talk-bubble tri-right round left-in">
+                <div className="talktext">
             <p className="feature__text">Made with passion and alot of stuff that I have placeholder </p>
+            </div>
+            </div>
         </div></div>
         <div className="feature"> <div className="feature">
            <img src={cleric} className="feature__icon" />
             <h4 className="feature__title">Tailorcrafted Bag</h4>
+            <div className="talk-bubble tri-right round left-in">
+                <div className="talktext">
             <p className="feature__text">All set of dices come with a special tailorcrafted bag, made by the great dookie</p>
+            </div>
+            </div>
         </div></div>
         <div className="feature"> <div className="feature">
            <img src={druid} className="feature__icon" />
             <h4 className="feature__title">Colorful & Resillences</h4>
+            <div className="talk-bubble tri-right round left-in">
+                <div className="talktext">
             <p className="feature__text">You can pick from quite variations of sets, make your choice!</p>
+            </div></div>
         </div></div>
         <div className="feature"> <div className="feature">
            <img src={wizard} className="feature__icon" />
             <h4 className="feature__title">Plastic Made</h4>
+            <div className="talk-bubble tri-right round left-in">
+                <div className="talktext">
             <p className="feature__text">Our dices are made with the best quality of POXICA you can find</p>
+        </div></div>
         </div></div>
         <div className="feature"> <div className="feature">
            <img src={fighter} className="feature__icon" />
             <h4 className="feature__title">Lawful Good Dwarf</h4>
+            <div className="talk-bubble tri-right round left-in">
+                <div className="talktext">
             <p className="feature__text">Mama Berissa is a lawful good dwarf priest who has helped alot of people</p>
+            </div></div>
         </div></div>
     </section>
 );
